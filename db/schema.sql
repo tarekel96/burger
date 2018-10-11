269ce05db5c5a1ba433308bcace64d76dfcb0b1e
Tarek ElHajjaoui <tarekel96@gmail.com>
2018-10-10 22:01:11 -0700

    added files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table burgers.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+-- Insert a set of records.
+INSERT INTO burgers (order) VALUES ('Double, double plain.');
+INSERT INTO burgers (order) VALUES ('Original with no cheese.');
+INSERT INTO burgers (order) VALUES ('Animal style cheeseburger.');
